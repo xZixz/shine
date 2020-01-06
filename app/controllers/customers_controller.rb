26e0db943d0db951408bebd9d9e5c83b4ed27c21
KienLe <kien.le.van.89@gmail.com>
2020-01-06 14:28:41 +0700

    Beautify list UI by Bootstrap

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,16 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords].strip()
       customer_search_term = CustomerSearchTerm.new @keywords
       @customers = Customer.where(customer_search_term.where_clause,
                                   customer_search_term.where_args)
                            .order(customer_search_term.order)
+                           .offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
-      @customers = Customer.all.limit(10)
+      @customers = []
     end
   end
 end
